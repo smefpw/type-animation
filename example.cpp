584ebf16fa3fd5a1090dfb826473f36c64e10520
justin <smefpw@gmail.com>
2019-12-07 01:01:54 +0000

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -8,14 +8,14 @@ int main()
 	// Change to make new line default.
 	//kb::kb_settings::default_endl = /*your custom value*/; // Accepts boolean
 	
-  // "\n" can be used to create multiple lines outputs, or to endl if default endl value is false
-  // This will use the default endl setting and char delay setting.
+	// "\n" can be used to create multiple lines outputs, or to endl if default endl value is false
+	// This will use the default endl setting and char delay setting.
 	kb::type_to_console("Big hackerman...");
   
-  // This will use a custom delay, but will still use default endl setting.
+	// This will use a custom delay, but will still use default endl setting.
 	kb::type_to_console("Big hackerman...", 125);
   
-  // This will use a custom delay and custom should endl;
+	// This will use a custom delay and custom should endl;
 	kb::type_to_console("Bighackerman...", 125, true);
 	
 	return 0;

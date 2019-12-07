d7f74b3d2c23d2c7a90f564708fe8d2c7c8d5ec0
justin <smefpw@gmail.com>
2019-12-07 00:53:54 +0000

    Create type_anim.h

---
+++ type_anim.h
@@ -0,0 +1,57 @@
+#pragma once
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+// TODO:: Refactor overrides
+
+namespace kb
+{
+	namespace kb_settings
+	{
+		inline unsigned default_dur = 75;
+		inline bool default_endl = false;
+	}
+
+	inline void type_to_console(const std::string_view str, const unsigned dur, const bool endl)
+	{
+		for (const auto c : str)
+		{
+			std::cout << c;
+			std::this_thread::sleep_for(std::chrono::milliseconds(dur));
+		}
+
+		if (endl)
+		{
+			std::cout << std::endl;
+		}
+	}
+	
+	inline void type_to_console(const std::string_view str, const unsigned dur)
+	{
+		for (const auto c : str)
+		{
+			std::cout << c;
+			std::this_thread::sleep_for(std::chrono::milliseconds(dur));
+		}
+
+		if (kb_settings::default_endl)
+		{
+			std::cout << std::endl;
+		}
+	}
+
+	inline void type_to_console(const std::string_view str)
+	{
+		for (const auto c : str)
+		{
+			std::cout << c;
+			std::this_thread::sleep_for(std::chrono::milliseconds(kb_settings::default_dur));
+		}
+
+		if (kb_settings::default_endl)
+		{
+			std::cout << std::endl;
+		}
+	}
+}
